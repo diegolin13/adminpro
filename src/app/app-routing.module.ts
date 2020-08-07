8522e64c134125b13c8c0be550ed2d8199ab27ec
Diego Juarez <diegoazulyoro13@gmail.com>
2020-08-07 15:01:02 -0500

    Se implementaron para mantener simple el app module y se incorporaron rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
-import {DashboardComponent} from "./pages/dashboard/dashboard.component";
-import {LoginComponent} from "./Auth/login/login.component";
-import {RegisterComponent} from "./Auth/register/register.component";
-import {ProgressComponent} from "./pages/progress/progress.component";
-import {Grafica1Component} from "./pages/grafica1/grafica1.component";
-import {NopagefoundComponent} from "./pages/nopagefound/nopagefound.component";
-import { PagesComponent } from './pages/pages.component';
+// Modulos
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthRoutingModule } from './Auth/register/auth-routing.module';
+
+import {NopagefoundComponent} from "./nopagefound/nopagefound.component";
+
+
 
 const routes: Routes = [
-  {path: '',
-  component: PagesComponent,
-children: [
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'progress', component: ProgressComponent},
-  {path: 'grafica1', component: Grafica1Component},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-]},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: '**', component: NopagefoundComponent}
+  {path: '**', component: NopagefoundComponent},
 ];
 
 @NgModule({
@@ -27,6 +18,8 @@ children: [
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule,
+     PagesRoutingModule,
+    AuthRoutingModule ]
 })
 export class AppRoutingModule { }
